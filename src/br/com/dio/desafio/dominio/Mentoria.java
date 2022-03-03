e40cc29fec36218ebc4e975ab1daacbcb2ddb2d6
Eduardo Torres <dutorres3108@hotmail>
2022-03-03 20:41:43 -0300

    Mentoria e Curso

---
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,46 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Mentoria {
+    private String titulo ;
+    private String descricao;
+    private LocalDateTime data;
+
+    public Mentoria() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
